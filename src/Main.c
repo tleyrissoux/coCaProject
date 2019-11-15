639bf3b13a3d6c006be71227e21819eac50ec741
tleyrissoux <46430877+tleyrissoux@users.noreply.github.com>
2019-11-15 18:26:09 +0100

    Add files via upload

--- src/Main.c
+++ src/Main.c
@@ -4,33 +4,57 @@
 #include "Solving.h"
 #include <string.h>
 
+void print_formula(Z3_context ctx, Z3_ast formula) {
+  printf("\nSAT formula : \n");
+  printf(Z3_ast_to_string(ctx, formula));
+}
+
+void print_graphes(Graph *graphs, int shift) {
+  for (int i = shift; i < argc; i++) {
+    printf("\nGraphe %d : \n", i-1);
+    printGraph(graphs[i-shift]);
+  }
+}
+
 int main(int argc, char* argv[])
 {
   Z3_context ctx = makeContext();
-  Z3_ast formule;
-  if (strcmp(argv[1], "-f") != 0 && strcmp(argv[1], "-g") != 0) {
+  Z3_ast formula;
+  int f1 = strcmp(argv[1], "-f");
+  int f2 = strcmp(argv[2], "-f");
+  int g1 = strcmp(argv[1], "-g");
+  int g2 = strcmp(argv[2], "-g");
+  if (f1 != 0 && g1 != 0) {
     Graph graphs[argc-1];
     for (int i = 1; i < argc; i++) {
       graphs[i-1] = getGraphFromFile(argv[i]);
     }
-    formule = graphsToFullFormula(ctx, graphs, argc-1);
+    formula = graphsToFullFormula(ctx, graphs, argc-1);
   }
   else {
-    Graph graphs[argc-2];
-    for(int i = 2; i < argc; i++) {
-      graphs[i-2] = getGraphFromFile(argv[i]);
-    }
-    formule = graphsToFullFormula(ctx, graphs, argc-2);
-    if (strcmp(argv[1], "-f") == 0) { //print formula
-      printf("\nSAT Formula : \n");
-      printf(Z3_ast_to_string(ctx, formule));
+    if ((f1 == 0 && g2 != 0) || (g1 == 0 && f2 != 0)) {
+      Graph graphs[argc-2];
+      for (int i = 2; i < argc; i++) {
+	graphs[i-2] = getGraphFromFile(argv[i]);
+      }
+      formula = graphsToFullFormula(ctx, graphs, argc-2);
+      if (f1 == 0 && g2 != 0) { //print formula
+	print_formula(ctx, formula);
+      }
+      else { //print graphes
+	print_graphes(graphs, 2);
+      }
     }
     else {
-      if (strcmp(argv[1], "-g") == 0) { //print graphes
-	for (int i = 2; i < argc; i++) {
-	  printf("\nGraphe %d : \n", i-1);
-	  printGraph(graphs[i-2]);
+      if ((g1 == 0 && f2 == 0) || (f1 == 0 && g2 == 0)) {
+	Graph graphs[argc-3];
+	for (int i = 3; i < argc; i++) {
+	  graphs[i-3] = getGraphFromFile(argv[i]);
 	}
+	formula = graphsToFullFormula(ctx, graphs, argc-3);
+	print_formula(ctx, formula);
+	printf("\n");
+	print_graphes(graphs, 3);
       }
       else {
 	printf("Unvalid parameters ! \n");
